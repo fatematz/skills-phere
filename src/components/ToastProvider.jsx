7f93c00526953393a4e2319ca0b8147bfc6e5e1e
Fatema <fatematz486@gmail.com>
2026-05-03 01:19:26 +0600

    Add Toast Provider

--- src/components/ToastProvider.jsx
+++ src/components/ToastProvider.jsx
@@ -3,7 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ToastProvider = () => {
-  return <ToastContainer position="top-center" />;
+  return <ToastContainer />;
 };
 
 export default ToastProvider;
